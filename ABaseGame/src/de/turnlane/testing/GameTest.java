e5e140824004699c10f816a6d78a9b86544f74d0
Fredie <Fredie@Linggify.fritz.box>
2014-11-17 18:41:22 +0100

    neue Material Definition Template gemacht, braucht kein flat shading mehr in Blender um die Modelle als Flatshading anzuzeigen.
    Test Szene von TheLastLeo um eine option erweitert: optimiert, und so wie TheLastLeo es vorher hatte (man macht das über einen boolean in GameTest.java
    
    Flycam in der Testszene schneller gemacht (sonst is des so ätzend damit rumzufliegen)
    
    optimierung reduziert den vertexcount in der szene auf etwa 5 - 20% des vorher benötigten (yay)
    
    lod in der Testszene funktioniert -> je nach system zwischen 2 - 10 fps mehr

--- ABaseGame/src/de/turnlane/testing/GameTest.java
+++ ABaseGame/src/de/turnlane/testing/GameTest.java
@@ -9,6 +9,7 @@ import com.jme3.math.Vector3f;
 import com.jme3.renderer.RenderManager;
 import com.jme3.scene.Node;
 import com.jme3.scene.Spatial;
+import com.jme3.scene.control.LodControl;
 
 /**
  * Ein Test mit meinem Baum. Generiert einen grünen Wald.
@@ -31,18 +32,42 @@ public class GameTest extends SimpleApplication {
         return model;
     }
     
+    private LodControl createLodControl() {
+        LodControl lc = new LodControl();
+        lc.setDistTolerance(10f);
+        lc.setTrisPerPixel(0.02f);
+        lc.setEnabled(true);
+        
+        return lc;
+    }
+    
     
     @Override
     public void simpleInitApp() {       
         terrain = assetManager.loadModel("Scenes/world.j3o");
         rootNode.attachChild(terrain);
+        
+        flyCam.setMoveSpeed(10);
 
         Vector2f forestBounds = new Vector2f(200f,200f);
         
-        Spatial[] treeModels = new Spatial[2];
-        treeModels[0] = loadModel("vegetation/Dreiastbaum/Dreiastbaum_1");
-        treeModels[1] = loadModel("vegetation/Dreiastbaum/Dreiastbaum_2");
-        Node trees = new SpatialDistributor(forestBounds,0.25f,treeModels).distribute();
+        Node[] treeModels = new Node[2];
+        boolean optimized = true;
+        
+        if(optimized) {
+            treeModels[0] = (Node) loadModel("vegetation/Dreiastbaum/optimiert/Dreiastbaum_1");
+            treeModels[0].getChild("Cube1").addControl(createLodControl());
+            treeModels[0].getChild("Cube2").addControl(createLodControl());
+            
+            treeModels[1] = (Node) loadModel("vegetation/Dreiastbaum/optimiert/Dreiastbaum_2");
+            treeModels[1].getChild("Cube.0011").addControl(createLodControl());
+            treeModels[1].getChild("Cube.0012").addControl(createLodControl());
+        } else {
+            treeModels[0] = (Node) loadModel("vegetation/Dreiastbaum/Dreiastbaum_1");
+            treeModels[1] = (Node) loadModel("vegetation/Dreiastbaum/Dreiastbaum_2");
+        }
+        Node trees = new SpatialDistributor(forestBounds,0.0625f,treeModels).distribute();
+        
         rootNode.attachChild(trees);
         
         // Spatial windmühle = loadModel("buildings/Windmühle");
